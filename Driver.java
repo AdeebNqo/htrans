951ddc5be62563924e83118d02639682f14695a3
AdeebNqo <adeebnqo@gmail.com>
2014-08-09 12:15:36 +0200

    added image save capability

--- Driver.java
+++ Driver.java
@@ -8,16 +8,22 @@ http://www.gnu.org/licenses/gpl-3.0.txt
 
 */
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 public class Driver{
 	public static void main(String[] args){
 		try{
 			File img0 = new File(args[0]);
 			BufferedImage img = ImageIO.read(img0);
 			ImageProcessor imgprocessor = new ImageProcessor();
-			imgprocessor.GaussianBlur(imgprocessor);
-	
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+	}
+	public static void saveImage(BufferedImage img, String filename, String extension) throws IOException{
+		File outputfile = new File(filename);
+		ImageIO.write(img, extension, outputfile);
 	}
 }
