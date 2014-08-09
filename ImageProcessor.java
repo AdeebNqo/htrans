2dad84ac7de3b831def0b51c0c24ab451417e001
AdeebNqo <adeebnqo@gmail.com>
2014-08-09 11:51:57 +0200

    added method for creating img from pixels

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -10,30 +10,62 @@ http://www.gnu.org/licenses/gpl-3.0.txt
 import java.awt.image.BufferedImage;
 public class ImageProcessor{
 
+	//boundary extension options
+	//public static int SYMETRIC = 0;
+	public static int PERIODIC = 1;
+	public static int ZERO = 2;
+	public static int CONTINUATION = 4;
+
 	public ImageProcessor(){
 
 	}
 
-	public BufferedImage GaussianBlur(BufferedImage img, int kernelsize,double sigma){
+	public BufferedImage GaussianBlur(BufferedImage img, int kernelsize, double sigma, int boundaryextension){
 		int w = img.getWidth();
 		int h = img.getHeight();
 
 		//retrieving the pixel values from the image
-		double[][] pixels = new double[w][h];
+		int[][] pixels = new int[w][h];
 		for (int i=0; i<w; ++i){
 			for (int j=0; j<h; ++j){
 				pixels[i][j] = img.getRGB(i,j);
 			}
 		}
 
+
+		double[] mask = get1dMask(kernelsize ,sigma);
 		/*
 
 		Applying the filter -- that is applying the dot product
 
+		x-direction
 		*/
-		double[] mask = get1dMask(kernelsize ,sigma);
+		for (int j=0; j<h; ++j){ //running through height
+			for (int i=0; i<w; ++i){ //running through width
+				double pixelproductsum = 0.0;
+				for (int maskindex=0; maskindex<kernelsize; ++maskindex){
+					double pixelval = Double.POSITIVE_INFINITY;
+					if (maskindex+i >= w){
+						//boundary extension
+						if (boundaryextension==ImageProcessor.ZERO){
+							pixelval = 0;
+						}
+						else if(boundaryextension==ImageProcessor.CONTINUATION){
+							pixelval = pixels[w-1][j];
+						}
+						else if (boundaryextension==ImageProcessor.PERIODIC){
+							pixelval = pixels[(maskindex+i)%w][j];
+						}
 
-		return null;
+					}else{
+						pixelval = pixels[maskindex+i][j];
+					}
+					pixelproductsum += mask[maskindex]*pixelval;
+				}
+				pixels[i][j] = (int) pixelproductsum;
+			}
+		}
+		return createImage(w, h, pixels);
 	}
 
 	public double[][] get2dMask(int width, int height, double sigma){
@@ -67,5 +99,19 @@ public class ImageProcessor{
 				mask[x] = mask[x] / total ;
 			}
 			return mask;
+	}
+	/*
+
+	Method for converting pixel array into an image
+
+	*/
+	public BufferedImage createImage(int width, int height, int[][] pixelvalues){
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		for (int x=0; x<width; ++x){
+			for(int y=0; y<height; ++y){
+				img.setRGB(x, y, pixelvalues[x][y]);
+			}
+		}
+		return img;
 	}
 }
